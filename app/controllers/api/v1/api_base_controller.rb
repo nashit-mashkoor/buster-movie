e68b35f60a9442132a8e03f5e8ea157c37a213a1
Nashit <l164104@lhr.nu.edu.pk>
2019-07-22 17:59:15 +0500

    Added user viewing only

--- app/controllers/api/v1/api_base_controller.rb
+++ app/controllers/api/v1/api_base_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ApiBaseController < ApplicationController
   attr_reader :current_user
-  #skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!
   protected
   def authenticate_request!
     unless user_id_in_token?
