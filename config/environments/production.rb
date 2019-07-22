04c17d1cd8aebc1c68a7f223607ddc901a2c9eb4
Nashit <l164104@lhr.nu.edu.pk>
2019-07-22 17:28:55 +0500

    Changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { host: 'https://buster-movie-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'buster-movie-staging.herokuapp.com' }
 
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :smtp
