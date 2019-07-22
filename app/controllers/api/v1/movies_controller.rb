16c87a69a95443136cb299649594c529b97fe344
Nashit <l164104@lhr.nu.edu.pk>
2019-07-22 14:49:22 +0500

    Added rails_12factor gem

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::MoviesController < Api::V1::ApiBaseController
   #before_action :authenticate_request!
   def index
-    if params[:search].present?
-      @parameter = params[:search].downcase  
-      @movies = Movie.where("title LIKE :search", search: @parameter).page(params[:page]).per(@per_page_count)
+   if params[:search].present?
+         @parameter = params[:search].downcase  
+         @movies = Movie.where("lower(title) like ?", "%#{@parameter}%") 
     else
-      @movies = Movie.all.page(params[:page]).per(@per_page_count)
+        @movies = Movie.all
     end
     render json: @movies
   end
