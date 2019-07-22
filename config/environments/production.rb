54163b071c881804cc5a32d19b09ffc9aa24c5d4
Nashit <l164104@lhr.nu.edu.pk>
2019-07-22 16:53:05 +0500

    Update error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { :host => 'buster-movie-staging.herokuapp.com' }
+
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
