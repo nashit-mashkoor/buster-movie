8926d66197bcc69d14ab7066ff632d0f9de5176c
Nashit <l164104@lhr.nu.edu.pk>
2019-07-25 11:59:41 +0500

    Implemented Custom exception pages for 404, 500 via errors controller Fixed the display of the pre-loader image

--- config/routes.rb
+++ config/routes.rb
@@ -37,5 +37,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'movies#home'
-
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_error'
 end
---
+++ test/controllers/errors_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ErrorsControllerTest < ActionDispatch::IntegrationTest
+  test "should get not_found" do
+    get errors_not_found_url
+    assert_response :success
+  end
+
+  test "should get internal_error" do
+    get errors_internal_error_url
+    assert_response :success
+  end
+
+end
